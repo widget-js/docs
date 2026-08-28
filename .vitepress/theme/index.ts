5a64dd31fe1bc58707bed3fa69f34f25472f278f
Neo Fu <rtugeek@gmail.com>
2026-08-28 15:52:36 +0800

    refactor: update project structure; remove unused files and add new components

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,7 +1,14 @@
+import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import BilibiliVideo from './components/BilibiliVideo.vue'
+import LogoTab from './components/LogoTab.vue'
+import PromptTypewriter from './components/PromptTypewriter.vue'
 
 export default {
   ...DefaultTheme,
-  enhanceApp(ctx: any) {
+  enhanceApp({ app }) {
+    app.component('BilibiliVideo', BilibiliVideo)
+    app.component('LogoTab', LogoTab)
+    app.component('PromptTypewriter', PromptTypewriter)
   },
-}
+} satisfies Theme
